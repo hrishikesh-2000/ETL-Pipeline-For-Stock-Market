e998c04d12442cc607ef70fb03e5035252ff5a9d
hrishikesh-2000 <sutarhrishikesh00@gmail.com>
2024-11-02 13:07:10 +0530

    Testing with git

--- historical_data_api_caller.py
+++ historical_data_api_caller.py
@@ -6,3 +6,17 @@ spark = SparkSession.builder.appName("Historical Data Caller").getOrCreate()
 
 
 # Historical candle data to retreive OHLC (Open, High, Low, Close) about give instrument
+
+
+url = "https://api.upstox.com/v2/historical-candle/:instrument_key/:interval/:to_date/:from_date"
+
+payload={}
+headers = {
+  'Accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
+## testing in intial dev
